f1cf6c07fa8dfcdcd5deb184e55da0f18487fbc6
Tesfaye Lemi <tesfu9503@gmail.com>
2022-11-21 00:11:55 -0800

    unknown error

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ import dotenv from "dotenv";
 import category from './routes/category.js'
 // import subCategory from './routes/subCategory.js'
 // import auth from './routes/auth.js'
-// import listings from './routes/listings.js'
-// import admin from './routes/admin.js'
-// import user from './routes/user.js'
+import listings from './routes/listings.js'
+import admin from './routes/admin.js'
+import user from './routes/user.js'
 import notification from './routes/notification.js'
 import banks from './routes/banks.js'
 import favorite from './routes/favorite.js'
@@ -31,9 +31,9 @@ app.use(express.json());
 // app.use('/api/category',category)
 // app.use('/api/subcategory',subCategory)
 // app.use('/api/auth',auth)
-// app.use('/api/listings',listings)
-// app.use('/api/admin',admin)
-// app.use('/api/users',user)
+app.use('/api/listings',listings)
+app.use('/api/admin',admin)
+app.use('/api/users',user)
 app.use('/api/notification',notification)
 app.use('/api/banks',banks)
 app.use('/api/favorite',favorite)
