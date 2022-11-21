c204d773acd4f2b12fc8fe685ebf25b811658200
Tesfaye Lemi <tesfu9503@gmail.com>
2022-11-20 23:41:21 -0800

    unknown error

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 //routes
-import category from './routes/category.js'
-import subCategory from './routes/subCategory.js'
-import auth from './routes/auth.js'
-import listings from './routes/listings.js'
-import admin from './routes/admin.js'
-import user from './routes/user.js'
-import notification from './routes/notification.js'
-import banks from './routes/banks.js'
-import favorite from './routes/favorite.js'
+// import category from './routes/category.js'
+// import subCategory from './routes/subCategory.js'
+// import auth from './routes/auth.js'
+// import listings from './routes/listings.js'
+// import admin from './routes/admin.js'
+// import user from './routes/user.js'
+// import notification from './routes/notification.js'
+// import banks from './routes/banks.js'
+// import favorite from './routes/favorite.js'
 
 dotenv.config();
 const app = express();
@@ -28,15 +28,15 @@ app.use(cors());
 app.use(express.json());
 
 //end point apis
-app.use('/api/category',category)
-app.use('/api/subcategory',subCategory)
-app.use('/api/auth',auth)
-app.use('/api/listings',listings)
-app.use('/api/admin',admin)
-app.use('/api/users',user)
-app.use('/api/notification',notification)
-app.use('/api/banks',banks)
-app.use('/api/favorite',favorite)
+// app.use('/api/category',category)
+// app.use('/api/subcategory',subCategory)
+// app.use('/api/auth',auth)
+// app.use('/api/listings',listings)
+// app.use('/api/admin',admin)
+// app.use('/api/users',user)
+// app.use('/api/notification',notification)
+// app.use('/api/banks',banks)
+// app.use('/api/favorite',favorite)
 app.get("/", (req, res) => {
   res.send("successs");
 });
