ba155bf35984a03cb2173b06049dfc52ea70ceb1
Tesfaye Lemi <tesfu9503@gmail.com>
2022-12-17 07:26:59 -0800

    deployment to railway configaration

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 //routes
-// import category from './routes/category.js'
-// import subCategory from './routes/subCategory.js'
-// import auth from './routes/auth.js'
-// import listings from './routes/listings.js'
-// import admin from './routes/admin.js'
-// import user from './routes/user.js'
-// import notification from './routes/notification.js'
-// import banks from './routes/banks.js'
-// import favorite from './routes/favorite.js'
-// import userContact from './routes/userContact.js'
+import category from './routes/category.js'
+import subCategory from './routes/subCategory.js'
+import auth from './routes/auth.js'
+import listings from './routes/listings.js'
+import admin from './routes/admin.js'
+import user from './routes/user.js'
+import notification from './routes/notification.js'
+import banks from './routes/banks.js'
+import favorite from './routes/favorite.js'
+import userContact from './routes/userContact.js'
 dotenv.config();
 const app = express();
 
@@ -28,16 +28,16 @@ app.use(cors());
 app.use(express.json());
 
 //end point apis
-// app.use('/api/category',category)
-// app.use('/api/subcategory',subCategory)
-// app.use('/api/auth',auth)
-// app.use('/api/listings',listings)
-// app.use('/api/admin',admin)
-// app.use('/api/users',user)
-// app.use('/api/notification',notification)
-// app.use('/api/banks',banks)
-// app.use('/api/favorite',favorite);
-// app.use('/api/contact-info',userContact)
+app.use('/api/category',category)
+app.use('/api/subcategory',subCategory)
+app.use('/api/auth',auth)
+app.use('/api/listings',listings)
+app.use('/api/admin',admin)
+app.use('/api/users',user)
+app.use('/api/notification',notification)
+app.use('/api/banks',banks)
+app.use('/api/favorite',favorite);
+app.use('/api/contact-info',userContact)
 
 
 
